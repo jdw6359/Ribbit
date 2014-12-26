f5a2335dc8c3032e00af5897f47f81699bf03a4b
Josh Woodward <josh_woodward@medisked.com>
2014-12-26 12:58:29 -0500

    Swipe Refresh gesture complete

--- app/src/main/java/woodward/joshua/ribbit/UI/InboxFragment.java
+++ app/src/main/java/woodward/joshua/ribbit/UI/InboxFragment.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -31,13 +33,19 @@ import woodward.joshua.ribbit.R;
 public class InboxFragment extends android.support.v4.app.ListFragment {
 
     protected List<ParseObject> mMessages;
-
+    protected SwipeRefreshLayout mSwipeRefreshLayout;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.inbox_fragment, container, false);
+
+        //because this is not an activity, we mush use findViewById on the rootView
+        mSwipeRefreshLayout=(SwipeRefreshLayout)rootView.findViewById(R.id.swipeRefreshLayout);
+        mSwipeRefreshLayout.setOnRefreshListener(mOnRefreshListener);
+        //set the color scheme of the "loader"
+        mSwipeRefreshLayout.setColorScheme(R.color.swipeRefresh1,R.color.swipeRefresh2,R.color.swipeRefresh3,R.color.swipeRefresh4);
         return rootView;
     }
 
@@ -46,42 +54,10 @@ public class InboxFragment extends android.support.v4.app.ListFragment {
         super.onResume();
 
         getActivity().setProgressBarIndeterminateVisibility(true);
+        retrieveMessages();
 
-        //query the Parse message class
-        //only pull messages where we are in the list of recipients.
-        ParseQuery<ParseObject> inboxQuery= new ParseQuery<ParseObject>(ParseConstants.CLASS_MESSAGES);
-        //Parse will do the work of looking at ALL the recipient ids and determine if current user is in set
-        inboxQuery.whereEqualTo(ParseConstants.KEY_RECIPIENT_IDS, ParseUser.getCurrentUser().getObjectId());
-        //orders results by created date, descending
-        inboxQuery.orderByDescending(ParseConstants.KEY_CREATED_AT);
-        inboxQuery.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> messages, ParseException e) {
-                getActivity().setProgressBarIndeterminateVisibility(false);
-                if(e==null){
-                    //successful, we found messages
-                    mMessages=messages;
-                    String[] usernames=new String[mMessages.size()];
-                    //initialize counter, and add ParseUser.username to list
-                    int i=0;
-                    for(ParseObject message: mMessages){
-                        usernames[i]=message.getString(ParseConstants.KEY_SENDER_NAME);
-                        i++;
-                    }
-
-                    //check to see if list view has an adapter
-                    if(getListView().getAdapter()==null){
-                        MessageAdapter inboxAdapter=new MessageAdapter(getListView().getContext(),mMessages);
-                        setListAdapter(inboxAdapter);
-                    }else{
-                        ((MessageAdapter)getListView().getAdapter()).refill(mMessages);
-                    }
-                }else{
-                    //fuuuuuuck
-                }
-            }
-        });
     }
+
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
@@ -124,5 +100,57 @@ public class InboxFragment extends android.support.v4.app.ListFragment {
             message.removeAll(ParseConstants.KEY_RECIPIENT_IDS,idsToRemove);
             message.saveInBackground();
         }
+    }
+
+    protected SwipeRefreshLayout.OnRefreshListener mOnRefreshListener=new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            retrieveMessages();
+        }
+    };
+
+    // method to retrieve and set messages in the list view
+    private void retrieveMessages() {
+        //query the Parse message class
+        //only pull messages where we are in the list of recipients.
+        ParseQuery<ParseObject> inboxQuery= new ParseQuery<ParseObject>(ParseConstants.CLASS_MESSAGES);
+        //Parse will do the work of looking at ALL the recipient ids and determine if current user is in set
+        inboxQuery.whereEqualTo(ParseConstants.KEY_RECIPIENT_IDS, ParseUser.getCurrentUser().getObjectId());
+        //orders results by created date, descending
+        inboxQuery.orderByDescending(ParseConstants.KEY_CREATED_AT);
+        inboxQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> messages, ParseException e) {
+                getActivity().setProgressBarIndeterminateVisibility(false);
+
+                //if refresh animation is occuring, kill it
+                if(mSwipeRefreshLayout.isRefreshing()){
+                    mSwipeRefreshLayout.setRefreshing(false);
+                }
+
+                //if message is valid, build list here
+                if(e==null){
+                    //successful, we found messages
+                    mMessages=messages;
+                    String[] usernames=new String[mMessages.size()];
+                    //initialize counter, and add ParseUser.username to list
+                    int i=0;
+                    for(ParseObject message: mMessages){
+                        usernames[i]=message.getString(ParseConstants.KEY_SENDER_NAME);
+                        i++;
+                    }
+
+                    //check to see if list view has an adapter
+                    if(getListView().getAdapter()==null){
+                        MessageAdapter inboxAdapter=new MessageAdapter(getListView().getContext(),mMessages);
+                        setListAdapter(inboxAdapter);
+                    }else{
+                        ((MessageAdapter)getListView().getAdapter()).refill(mMessages);
+                    }
+                }else{
+                    //didnt find any messages
+                }
+            }
+        });
     }
 }
