103feae84628c9f6af5fdb89c5c34d89b503126f
Josh Woodward <josh_woodward@medisked.com>
2014-12-25 23:23:48 -0500

    Auth pages completed

--- app/src/main/java/woodward/joshua/ribbit/UI/SignUpActivity.java
+++ app/src/main/java/woodward/joshua/ribbit/UI/SignUpActivity.java
@@ -23,6 +23,7 @@ public class SignUpActivity extends Activity {
     protected EditText mPassword;
     protected EditText mEmail;
     protected Button mSignUpButton;
+    protected Button mCancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +39,17 @@ public class SignUpActivity extends Activity {
         mUsername=(EditText)findViewById(R.id.usernameField);
         mPassword=(EditText)findViewById(R.id.passwordField);
         mEmail=(EditText)findViewById(R.id.emailField);
-        mSignUpButton=(Button)findViewById(R.id.signUpButton);
 
+        mCancelButton=(Button)findViewById(R.id.cancelButton);
+        mCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //return the user to the sign in activity
+                finish();
+            }
+        });
+
+        mSignUpButton=(Button)findViewById(R.id.signUpButton);
         //listener for sign up button
         mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
