c51c09229c6cabf9df6488bd24de844c33bdc311
Josh Woodward <josh_woodward@medisked.com>
2014-12-26 19:32:59 -0500

    Progress with friends grid layout and adapter

---
+++ app/src/main/java/woodward/joshua/ribbit/Model/UserAdapter.java
@@ -0,0 +1,88 @@
+package woodward.joshua.ribbit.Model;
+
+import android.content.Context;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.Date;
+import java.util.List;
+
+import woodward.joshua.ribbit.R;
+
+/**
+ * Created by Joshua on 12/24/2014.
+ */
+public class UserAdapter extends ArrayAdapter<ParseUser> {
+
+    protected Context mContext;
+    protected List<ParseUser> mUsers;
+
+    public UserAdapter(Context context, List<ParseUser> users) {
+        super(context, R.layout.message_item,users);
+
+        //assign member variables
+        mContext=context;
+        mUsers =users;
+    }
+
+    //we need to override this so that we are utilizing and inflating message_item.xml
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        //follow 'ViewHolder' design pattern to adapt custom list layout
+        ViewHolder holder;
+        if(convertView==null){
+            //create it for the first time
+            convertView= LayoutInflater.from(mContext).inflate(R.layout.user_item,null);
+            holder=new ViewHolder();
+            holder.userImageView=(ImageView)convertView.findViewById(R.id.userImageView);
+            holder.nameLabel=(TextView)convertView.findViewById(R.id.nameLabel);
+            convertView.setTag(holder);
+        }else{
+            //it already exists, we just need to change the data
+            holder=(ViewHolder)convertView.getTag();
+        }
+
+        //get the message object in question
+        ParseUser user= mUsers.get(position);
+
+        String email=user.getEmail().toLowerCase();
+        if(email.equals("")){
+            //use the default image
+            holder.userImageView.setImageResource(R.drawable.avatar_empty);
+        }else{
+            String hash=MD5Util.md5Hex(email);
+            
+        }
+
+
+
+
+        holder.nameLabel.setText(user.getUsername());
+
+        return convertView;
+    }
+
+    //follows view holder pattern such that view holder object will hold the state
+    private static class ViewHolder{
+        ImageView userImageView;
+        TextView nameLabel;
+
+    }
+
+    public void refill(List<ParseUser> users){
+        //clear current data
+        mUsers.clear();
+        mUsers.addAll(users);
+        notifyDataSetChanged();
+    }
+
+}
--- app/src/main/java/woodward/joshua/ribbit/UI/FriendsFragment.java
+++ app/src/main/java/woodward/joshua/ribbit/UI/FriendsFragment.java
@@ -2,12 +2,15 @@ package woodward.joshua.ribbit.UI;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import com.parse.FindCallback;
 import com.parse.Parse;
@@ -19,23 +22,31 @@ import com.parse.ParseUser;
 import java.util.List;
 
 import woodward.joshua.ribbit.Model.ParseConstants;
+import woodward.joshua.ribbit.Model.UserAdapter;
 import woodward.joshua.ribbit.R;
 
 /**
  * Created by Joshua on 12/16/2014.
  */
-public class FriendsFragment extends ListFragment {
+public class FriendsFragment extends Fragment {
 
     public static final String TAG=FriendsFragment.class.getSimpleName();
 
     protected ParseRelation<ParseUser> mFriendsRelation;
     protected ParseUser mCurrentUser;
     protected List<ParseUser> mFriends;
+    protected GridView mGridView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.friends_fragment, container, false);
+
+        mGridView=(GridView)rootView.findViewById(R.id.friendsGrid);
+        TextView emptyTextView=(TextView)rootView.findViewById(android.R.id.empty);
+        //attach this as the empty text view for the grid view
+        mGridView.setEmptyView(emptyTextView);
+
         return rootView;
     }
 
@@ -67,13 +78,17 @@ public class FriendsFragment extends ListFragment {
                         usernames[i]=user.getUsername();
                         i++;
                     }
-                    //Create an array adapter of type String, give params (Context, ListType, Source)
-                    ArrayAdapter<String> friendsAdapter=new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_list_item_1, usernames);
-                    //setListAdapter() is method of ListActivity (the class this activity inherits from)
-                    setListAdapter(friendsAdapter);
+                    if(mGridView.getAdapter()==null){
+                        //Create an array adapter of type String, give params (Context, ListType, Source)
+                        UserAdapter friendsAdapter =new UserAdapter(getActivity(),mFriends);
+                        //sets the adapter for the grid view in this fragment
+                        mGridView.setAdapter(friendsAdapter);
+                    }else{
+                        ((UserAdapter)mGridView.getAdapter()).refill(mFriends);
+                    }
                 }else{
                     Log.e(TAG,e.getMessage());
-                    AlertDialog.Builder friendsListAlertBuilder=new AlertDialog.Builder(getListView().getContext());
+                    AlertDialog.Builder friendsListAlertBuilder=new AlertDialog.Builder(getActivity());
                     friendsListAlertBuilder.setTitle(getString(R.string.friends_list_error_title));
                     friendsListAlertBuilder.setMessage((getString(R.string.friends_list_error_message)));
                     friendsListAlertBuilder.setPositiveButton(android.R.string.ok,null);
